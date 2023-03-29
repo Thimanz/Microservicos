33ce5d3aaa4068cf4847ec2461c19ca3a6523c10
Thiago Manzi <thiago_manzi@hotmail.com>
2023-03-29 15:54:27 -0300

    apostila03(mss): fim do codigo 4.3.1

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+//GET localhost:400/lembretes
+app.get("/lembretes", (req, res) => {});
+
+//POST localhost:400/lembretes
+app.post("/lembretes", (req, res) => {});
+
+app.listen();
