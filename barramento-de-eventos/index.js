835f55887251157b3dd90c37a606593b67b8d8b8
Thiago Manzi <thiago_manzi@hotmail.com>
2023-05-27 02:23:55 -0300

    build: alteracoes no barramento para integracao com fastAPI do microsservico de sentimentos em python

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -4,23 +4,23 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
+const { BARRAMENTO_PORTA, BARRAMENTO_HOSTS } = process.env;
+
 app.post("/eventos", (req, res) => {
     const evento = req.body;
 
-    axios.post("http://localhost:4000/eventos", evento);
+    axios.post(`http://${BARRAMENTO_HOSTS}:4000/eventos`, evento);
 
-    axios.post("http://localhost:5000/eventos", evento);
+    axios.post(`http://${BARRAMENTO_HOSTS}:5000/eventos`, evento);
 
-    axios.post("http://localhost:6000/eventos", evento);
+    axios.post(`http://${BARRAMENTO_HOSTS}:6000/eventos`, evento);
 
-    axios.post("http://localhost:7000/eventos", evento);
+    axios.post(`http://${BARRAMENTO_HOSTS}:7000/eventos`, evento);
 
-    axios.post("http://localhost:8000/eventos", evento);
+    axios.post(`http://${BARRAMENTO_HOSTS}:8000/eventos`, evento);
 
     res.status(200).send({ msg: "ok" });
 });
-
-const { BARRAMENTO_PORTA } = process.env;
 
 app.listen(BARRAMENTO_PORTA, () =>
     console.log(`Barramento. ${BARRAMENTO_PORTA}`)
